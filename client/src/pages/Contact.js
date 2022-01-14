be5cea762258ca7810352768457757e9ce3047be
pukhraj1999 <pukhrajkhinda@gmail.com>
2022-01-15 00:12:32 +0530

    Contact fixing

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -53,11 +53,18 @@ function Contact() {
               <div className="col-6 col-lg-6 contact__left">
                 <div className="contact__left__box">
                   <p>ADDRESS</p>
-                  <p>BK DUTT GATE, AMRITSAR, PUNJAB, IN 143001</p>
+                  <a
+                    target="__blank"
+                    href="https://www.google.com/maps/place/Bala+Ji+Refrigeration+Works/@31.621764,74.866744,19z/data=!4m5!3m4!1s0x0:0xcfceb25f0cf9e679!8m2!3d31.6217637!4d74.8667439?hl=en"
+                  >
+                    BK DUTT GATE, AMRITSAR, PUNJAB, IN 143001
+                  </a>
                   <p>PHONE</p>
-                  <p>+91-95014-46674</p>
+                  <a href="tel:+919501446674">+91-95014-46674</a>
                   <p>MAIL</p>
-                  <p>CONTACT@ THEGRAPHIC SOCIAL .COM</p>
+                  <a href="mailto:CONTACT@THEGRAPHICSOCIAL.COM">
+                    CONTACT@ THEGRAPHIC SOCIAL .COM
+                  </a>
                 </div>
               </div>
               <div className="col-6 col-lg-6 d-flex flex-column  contact__right">
